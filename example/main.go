b5564f8a652eb0354b8828627a7cc457db8c8d04
Akshay Khanna <akshay.khanna@thoughtworks.com>
2018-11-20 22:19:09 +0530

    Remove commented code & fixed indentaion

--- example/main.go
+++ example/main.go
@@ -9,10 +9,7 @@ type myHandler struct {
 	greetings string
 }
 
-func (mh myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-}
 func main() {
-	//http.Handle("/", &myHandler{"Hello"})
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte(fmt.Sprintf("hello world", )))
 	})
